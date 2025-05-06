de4b8e951ff752904a4762b6a2f2a6448390bd1c
Rijan77 <rijanacharya73@gmail.com>
2025-05-06 12:37:37 +0545

    Initial design of homepage

--- lib/pages/MyHomePage.dart
+++ lib/pages/MyHomePage.dart
@@ -5,6 +5,10 @@ final helloProvider = Provider<String>((ref){
   return ("What can I help with?");
 });
 
+// final iconprovider = Provider((ref){
+//   return Icon(Icons.settings, size: 40, color: Colors.white,);
+// });
+
 class Myhomepage extends ConsumerStatefulWidget{
   const Myhomepage({super.key});
 
@@ -16,35 +20,79 @@ class _MyhomepageState extends ConsumerState<Myhomepage> {
   @override
   Widget build(BuildContext context) {
     final hello = ref.watch(helloProvider);
+    // final icon = ref.watch(iconprovider);
     return Scaffold(
       backgroundColor: const Color(0xff1E1E1E),
-      body: Column(
-        children: [
-          Row(
-              children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30, left: 10),
-                      child: Image.asset(
-                        "lib/assets/Logo.png", width: 40,
-                        fit: BoxFit.contain,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+                children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30, left: 10),
+                        child: Image.asset(
+                          "lib/assets/Logo.png", width: 40,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 100, top: 40),
+                    child: Text("GuffGaff", style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 100, top: 30),
+                    child: Icon(Icons.settings, color: Colors.white, size: 35,)
+                  )
+                ],
+              ),
+            SizedBox(height: 300,),
+
+            Text(hello, style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+            ),),
+
+        SizedBox(height: 290,),
+            Container(
+              // padding: EdgeInsets.symmetric(horizontal: 10),
+              height: 130,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.black26,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))
+              ),
+
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: "Ask any things",
+                        hintStyle: TextStyle(
+                          color: Colors.grey[400],
+                          fontWeight: FontWeight.w400,
+                        ),
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+
+                        contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                       ),
                     ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 100, top: 40),
-                  child: Text("GuffGaff", style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500
-                  ),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 100, top: 30),
-                  child: Icon(Icons.settings, color: Colors.white, size: 35,),
-                )
-              ],
-            ),
+                  ),
+                  SizedBox(width: 10,),
+                  Icon(Icons.send, color: Colors.white)
+                ],
+              )
+            )
 
-        ],
+          ],
+        ),
       ),
     );
   }
